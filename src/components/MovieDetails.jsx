5abae30edff0b320476a111a19b3f4e4858286e8
Nico_le <156796855+NicoleMaini@users.noreply.github.com>
2024-03-21 12:12:01 +0100

    fetch complete

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Spinner } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const MovieDetails = function () {
   // intanto settiamo uno stato a null
-  const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState("");
+  const [comments, setComments] = useState("");
 
   // recuperiamo il parametro che ci siamo passati dalla route parametrica
   const params = useParams(); // qui cì+ il valore del movieId
   console.log(params.movieId);
 
   const urlOmdb = `http://www.omdbapi.com/?apikey=d148c825&i=${params.movieId}`;
-  const strVoide = "";
 
   const urlStruive = `https://striveschool-api.herokuapp.com/api/comments/${params.movieId}`;
   const authorization = {
@@ -21,8 +21,8 @@ const MovieDetails = function () {
       "Content-Type": "application/json",
     },
   };
-  const fetchFun = (urlOmdb, authorization) => {
-    fetch(urlOmdb)
+  const fetchFun = (urlOmdb, func, authorization) => {
+    fetch(urlOmdb, authorization)
       .then(resp => {
         if (resp.ok) {
           return resp.json();
@@ -30,7 +30,7 @@ const MovieDetails = function () {
       })
       .then(info => {
         console.log(info);
-        setMovie(info);
+        func(info);
       })
       .catch(error => {
         console.log("Errore", error);
@@ -38,37 +38,45 @@ const MovieDetails = function () {
   };
 
   useEffect(() => {
-    fetchFun(urlOmdb, strVoide);
+    fetchFun(urlOmdb, setMovie);
+    fetchFun(urlStruive, setComments, authorization);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params.movieId]);
 
   console.log(movie);
+  console.log(comments);
 
   return (
-    <Col className="p-2 w-75 mx-auto my-5">
-      <Card className="flex-row 1px solid gray">
-        <div style={{ width: "11rem", height: "14rem", overflow: "hidden" }}>
-          <Card.Img
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              borderTopRightRadius: 0,
-              borderBottomLeftRadius: 5,
-            }}
-            variant="top"
-            src={movie.Poster}
-          />
-        </div>
-        <Card.Body className="col-8 d-flex flex-column">
-          <Card.Title className="display-5">{movie.Title}</Card.Title>
-          <div>
-            <Card.Text>
-              {} | {}
-            </Card.Text>
-          </div>
-        </Card.Body>
-      </Card>
-    </Col>
+    <>
+      {movie === null && <Spinner />}
+      {movie.imdbID === params.movieId && (
+        <Col className="p-2 w-75 mx-auto my-5">
+          <Card className="flex-row 1px solid gray">
+            <div style={{ width: "11rem", height: "14rem", overflow: "hidden" }}>
+              <Card.Img
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  borderTopRightRadius: 0,
+                  borderBottomLeftRadius: 5,
+                }}
+                variant="top"
+                src={movie.Poster}
+              />
+            </div>
+            <Card.Body className="col-8 d-flex flex-column">
+              <Card.Title className="display-5">{movie.Title}</Card.Title>
+              <div>
+                <Card.Text>
+                  {} | {}
+                </Card.Text>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      )}
+    </>
   );
 };
 
